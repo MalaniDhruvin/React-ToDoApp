a95cc87d0c09ab5ed88f553bc7e1bb14e08f8be5
Dhruvin Malani <malanidhruvin21@gmail.com>
2024-01-20 09:40:59 +0530

    Modification Added

--- src/componets/AddToDo.jsx
+++ src/componets/AddToDo.jsx
@@ -1,35 +1,46 @@
-import { useState } from "react";
+import { useRef } from "react";
 
 function AddToDo({ addButton }) {
 
-  const [itemName, setName] = useState();
-  const [itemDate, setDate] = useState();
-  const hundleInput = (event) => {
-    // console.log(event.target.value);
-    setName(event.target.value);
+  // const [itemName, setName] = useState();
+  // const [itemDate, setDate] = useState();
+  const ItemName=useRef();
+  const ItemDate=useRef();
+  // const hundleInput = (event) => {
+  //   console.log(event.target.value);
+  //   setName(event.target.value);
 
-  }
-  const hundleDate = (event) => {
-    // console.log(event.target.value);
-    setDate(event.target.value);
+  // }
+  // const hundleDate = (event) => {
+  //   console.log(event.target.value);
+  //   setDate(event.target.value);
 
-  }
+  // }
 
   const hundleAdd = () => {
-    addButton(itemName, itemDate);
-    setDate("");
-    setName("");
+    const itemname=ItemName.current.value;
+    const itemdate=ItemDate.current.value;
+    addButton(itemname, itemdate);
+    ItemName.current.value="";
+    ItemDate.current.value="";
+    // setDate("");
+    // setName("");
   }
   return (
     <div class="container main-container">
 
       <div class="row">
         <div class="col-6">
-          <input type="text" value={itemName} onChange={() => hundleInput(event)}
+          <input type="text"  
+          ref={ItemName}
+          // onChange={() => hundleInput(event)}
             placeholder="Enter Your ToDo Here" />
         </div>
         <div class="col-4">
-          <input type="date" value={itemDate} onChange={() => hundleDate(event)} />
+          <input type="date" 
+          ref={ItemDate}
+          // value={itemDate} onChange={() => hundleDate(event)} 
+          />
         </div>
         <div class="col-2">
           <button type="button" onClick={hundleAdd}
